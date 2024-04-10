91260e957f251c436680f39e13280e06d646f266
Dylan Thomas <dturbyville@gmail.com>
2024-04-10 16:32:41 -0400

    Test HTTPS API - 4.10.24 - Sprint 7 Project

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-65c1a314-36a0-4a00-8197-1b24f0f61acc.containerhub.tripleten-services.com"}
+    API_URL: "https://cnt-3f3edbd3-33cd-494b-a000-b3cdb50d57ce.containerhub.tripleten-services.com"}
